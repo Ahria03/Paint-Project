32384f1b6997bca26da20fafc33e7f60d6bad359
Teodor <90730116+Ahria03@users.noreply.github.com>
2022-01-02 03:15:15 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -7,17 +7,8 @@ run = True # Always initially True
 clock = pygame.time.Clock() # Initialize clock
 grid = init_grid(ROWS, COLS, BG_COLOR) # Initialize grid
 drawing_color = BLACK
-
-button_y = HEIGHT - TOOLBAR_HEIGHT/2 - 25
-buttons = [
-    # List of Buttons
-    Button(10, button_y, 50, 50, BLACK),
-    Button(70, button_y, 50, 50, RED),
-    Button(130, button_y, 50, 50, GREEN),
-    Button(190, button_y, 50, 50, BLUE),
-    Button(250, button_y, 50, 50, WHITE, "Erase", BLACK),
-    Button(310, button_y, 50, 50, WHITE, "Clear", BLACK)
-]
+current_shades = []
+brush_size = 1
 
 while run:
     clock.tick(FPS) # Only move as fast as FPS
@@ -29,20 +20,32 @@ while run:
         
         if pygame.mouse.get_pressed()[0]: # If user Left Clicks
             pos = pygame.mouse.get_pos() # Get X,Y Position of Spot Clicked
-            try:
-                row, col = get_row_col_from_pos(pos)
-                grid[row][col] = drawing_color
+            try: 
+                brush(brush_size, pos, grid, drawing_color)
             except IndexError:
-                for button in buttons:
-                    if not button.clicked(pos):
-                        continue
-                    
-                    drawing_color = button.color
-                    if button.text == "Clear":
+                
+                # We've clicked off the drawing, check if we clicked either Button or Shade Button
+                is_color, button = check_button_clicked(buttons, pos)
+                is_shade, shade = check_button_clicked(current_shades, pos)
+                
+                if is_color:
+                    # If button is Clear, Reset Grid
+                    if button.text == "CL":
                         grid = init_grid(ROWS, COLS, BG_COLOR)
                         drawing_color = BLACK
-                        
-                        
-    draw(WIN, grid, buttons) # Draw screen
+                        current_shades = []
+                    # If button is Erase
+                    elif button.text == "ER":
+                        drawing_color = WHITE
+                    # If button changes size
+                    if button.change_size():
+                        brush_size = button.size
+                    # Else its a main color so it opens shades
+                    else:
+                        current_shades = button.shade
+                elif is_shade:
+                    drawing_color = shade.color
+                              
+    draw(WIN, grid, buttons, current_shades) # Draw screen
             
 pygame.quit()
