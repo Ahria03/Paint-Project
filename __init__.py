c327696efa98e0a8e1344ee3b481741129f6b2f5
Teodor <90730116+Ahria03@users.noreply.github.com>
2022-01-02 03:18:29 -0500

    Delete __init__.py

--- __init__.py
+++
@@ -1,8 +0,0 @@
-from .settings import * # Period tells Program to import from folder
-from .drawing import *
-from .button import *
-import pygame
-
-# Initialize Pygame and its Font library
-pygame.init()
-pygame.font.init()
